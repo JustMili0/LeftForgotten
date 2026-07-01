18c2295bf4872f6806a9c7b38e62dd65f419a6d5
JustMili <rodx160@gmail.com>
2026-07-01 02:04:06 +0200

    Change cloud height to prevent them from appearing below the bedrock on Forge/Neo

--- common/src/main/java/net/justmili/leftforgotten/client/dimension/AlphaMinecraft.java
+++ common/src/main/java/net/justmili/leftforgotten/client/dimension/AlphaMinecraft.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class AlphaMinecraft extends DimensionSpecialEffects {
     public AlphaMinecraft() {
-        super(Float.NaN, true, SkyType.NORMAL, false, false);
+        super(255, true, SkyType.NORMAL, false, false);
     }
     @Override
     public @NotNull Vec3 getBrightnessDependentFogColor(@NotNull Vec3 color, float sunHeight) {
